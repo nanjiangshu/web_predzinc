30092844b35c83f29a071b4eeee597cb0c5af923
nanjiangshu <nanjiang.shu@gmail.com>
2021-02-27 17:17:42 +0100

    update urls.py

--- proj/pred/urls.py
+++ proj/pred/urls.py
@@ -1,18 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 #import modules for spyne (wsdl interface)
-# from spyne.protocol.soap import Soap11
-# from spyne.server.django import DjangoView
+from spyne.protocol.soap import Soap11
+from spyne.server.django import DjangoView
 
 #import modules for authentication
 from django.contrib.auth.decorators import login_required
 
 from proj.pred import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.index, name='pred.index'),
-    url(r'^search-form/$', views.search_form, name='pred.search_form'),
-    url(r'^search/$', views.search, name='pred.search'),
     url(r'^submit-seq/$', views.submit_seq, name='pred.submit_seq'),
     url(r'^thanks/$', views.thanks, name='pred.thanks'),
     url(r'^queue/$', views.get_queue, name='pred.get_queue'),
@@ -23,7 +21,9 @@ urlpatterns = patterns('',
     url(r'^help-wsdl-api/$', views.help_wsdl_api, name='pred.help_wsdl_api'),
     url(r'^help/$', views.get_help, name='pred.get_help'),
     url(r'^news/$', views.get_news, name='pred.get_news'),
+    url(r'^findjob/$', views.findjob, name='pred.findjob'),
     url(r'^serverstatus/$', views.get_serverstatus, name='pred.get_serverstatus'),
+    url(r'^countjobcountry/$', views.get_countjob_country, name='pred.get_countjob_country'),
     url(r'^reference/$', views.get_reference, name='pred.get_reference'),
     url(r'^example/$', views.get_example, name='pred.get_example'),
     url(r'^oldtopcons/$', views.oldtopcons, name='pred.oldtopcons'),
@@ -33,8 +33,8 @@ urlpatterns = patterns('',
     url(r'^login/', login_required(views.login), name="pred.login"),
 
 # for spyne wsdl
-    #url(r'^api_submitseq/', DjangoView.as_view(application=views.app_submitseq)),
+    url(r'^api_submitseq/', DjangoView.as_view(application=views.app_submitseq)),
 
-)
+]
 
 
